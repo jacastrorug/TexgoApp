7d10e532b6ebae740c05f1b65d641b1b187504d1
jacastrorug <juliancastroruge@gmail.com>
2021-03-25 18:15:23 -0500

    New fixs and dependencies

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -6,6 +6,19 @@ import Breadcumb from "../components/breadcumb";
 import Contactpage from "../components/Contactpage";
 
 const ContactPage = () => {
+
+  useEffect(() => {
+    // fetch("/.netlify/functions/send-contact-mail",
+    //   {
+    //     method: "POST", mode: "cors", cache: "no-cache",
+    //     credentials: "same-origin", headers: { "Content-Type": "application/json" },
+    //     redirect: "follow", referrerPolicy: "no-referrer",
+    //     body: JSON.stringify({ name: 'Julian', email: 'abcd@gmail.com' })
+    //   })
+    //   .then(res => res.json())
+    //   .then(data => console.log(data));
+  }, []);
+
   return (
     <Layout>
       <SEO title="Contact" />
--- src/pages/quote-road-service.js
+++ src/pages/quote-road-service.js
@@ -3,14 +3,14 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Breadcumb from "../components/breadcumb";
-import CuoteForm from "../components/CuoteForm";
+import QuoteForm from "../components/QuoteForm";
 
 const QuoteRoadPage = () => {
   return (
     <Layout>
       <SEO title="Quote Road Service" />
       <Breadcumb bdtitle={"Quote Road Service"} bdsub={"Quote"} />
-      <CuoteForm />
+      <QuoteForm />
     </Layout>
   );
 };
